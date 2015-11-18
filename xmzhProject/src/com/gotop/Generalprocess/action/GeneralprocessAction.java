f9f7b8117a43560470bc70936c929112da1f89ae
吴佳俊 <398907877@qq.com>
2015-11-18 14:41:35 +0800

    add  process

---
+++ xmzhProject/src/com/gotop/Generalprocess/action/GeneralprocessAction.java
@@ -0,0 +1,17 @@
+package com.gotop.Generalprocess.action;
+
+import com.gotop.crm.util.BaseAction;
+
+public class GeneralprocessAction   extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+	
+	
+}
---
+++ xmzhProject/src/com/gotop/Generalprocess/util/GeneralprocessUtil.java
@@ -0,0 +1,16 @@
+package com.gotop.Generalprocess.util;
+
+
+
+/**
+ * 
+ * @author wujiajun
+ *
+ */
+public class GeneralprocessUtil {
+	
+	
+	
+	
+
+}
