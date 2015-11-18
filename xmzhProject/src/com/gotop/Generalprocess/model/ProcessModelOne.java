a7af39f408ed9208f87d05a93eafc1718e96f025
吴佳俊 <398907877@qq.com>
2015-11-18 11:06:18 +0800

    节点模式  1，2   ，基本流程设计

---
+++ xmzhProject/src/com/gotop/Generalprocess/model/ProcessModelOne.java
@@ -0,0 +1,194 @@
+package com.gotop.Generalprocess.model;
+
+import java.io.Serializable;
+
+
+/**
+ * 
+ * @author wujiajun
+ * @desc  节点模式1 的实体类  
+ * @version 1.0
+ * 
+ **/
+public class ProcessModelOne  implements  Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+	private  Integer   processModelOneID;
+	private  Integer  flow_Id;
+	private  String   orgCodeOne;
+	private  String   orgCodeTwo;
+	private String  creator;
+	private String  fdxdy;
+	private String  yxzg;
+	private String  yxry;
+	private String  currency;
+	private String  apply_bal;
+	private String  oneCategory;
+	private String   loanCategory;
+	private String   dywx_Name;
+	private String   coOrganization;
+	private String   survey_Time;
+	private String  content;
+	private String   cust_Name;
+	
+	private String    create_Date;
+	private String    create_time;
+	private String     create_name;
+	
+	
+	private String last_up_date;
+	private String last_up_time;
+	private String    last_up_name;
+	public Integer getProcessModelOneID() {
+		return processModelOneID;
+	}
+	public void setProcessModelOneID(Integer processModelOneID) {
+		this.processModelOneID = processModelOneID;
+	}
+	public Integer getFlow_Id() {
+		return flow_Id;
+	}
+	public void setFlow_Id(Integer flow_Id) {
+		this.flow_Id = flow_Id;
+	}
+	public String getOrgCodeOne() {
+		return orgCodeOne;
+	}
+	public void setOrgCodeOne(String orgCodeOne) {
+		this.orgCodeOne = orgCodeOne;
+	}
+	public String getOrgCodeTwo() {
+		return orgCodeTwo;
+	}
+	public void setOrgCodeTwo(String orgCodeTwo) {
+		this.orgCodeTwo = orgCodeTwo;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getFdxdy() {
+		return fdxdy;
+	}
+	public void setFdxdy(String fdxdy) {
+		this.fdxdy = fdxdy;
+	}
+	public String getYxzg() {
+		return yxzg;
+	}
+	public void setYxzg(String yxzg) {
+		this.yxzg = yxzg;
+	}
+	public String getYxry() {
+		return yxry;
+	}
+	public void setYxry(String yxry) {
+		this.yxry = yxry;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getApply_bal() {
+		return apply_bal;
+	}
+	public void setApply_bal(String apply_bal) {
+		this.apply_bal = apply_bal;
+	}
+	public String getOneCategory() {
+		return oneCategory;
+	}
+	public void setOneCategory(String oneCategory) {
+		this.oneCategory = oneCategory;
+	}
+	public String getLoanCategory() {
+		return loanCategory;
+	}
+	public void setLoanCategory(String loanCategory) {
+		this.loanCategory = loanCategory;
+	}
+	public String getDywx_Name() {
+		return dywx_Name;
+	}
+	public void setDywx_Name(String dywx_Name) {
+		this.dywx_Name = dywx_Name;
+	}
+	public String getCoOrganization() {
+		return coOrganization;
+	}
+	public void setCoOrganization(String coOrganization) {
+		this.coOrganization = coOrganization;
+	}
+	public String getSurvey_Time() {
+		return survey_Time;
+	}
+	public void setSurvey_Time(String survey_Time) {
+		this.survey_Time = survey_Time;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCust_Name() {
+		return cust_Name;
+	}
+	public void setCust_Name(String cust_Name) {
+		this.cust_Name = cust_Name;
+	}
+	public String getCreate_Date() {
+		return create_Date;
+	}
+	public void setCreate_Date(String create_Date) {
+		this.create_Date = create_Date;
+	}
+	public String getCreate_time() {
+		return create_time;
+	}
+	public void setCreate_time(String create_time) {
+		this.create_time = create_time;
+	}
+	public String getCreate_name() {
+		return create_name;
+	}
+	public void setCreate_name(String create_name) {
+		this.create_name = create_name;
+	}
+	public String getLast_up_date() {
+		return last_up_date;
+	}
+	public void setLast_up_date(String last_up_date) {
+		this.last_up_date = last_up_date;
+	}
+	public String getLast_up_time() {
+		return last_up_time;
+	}
+	public void setLast_up_time(String last_up_time) {
+		this.last_up_time = last_up_time;
+	}
+	public String getLast_up_name() {
+		return last_up_name;
+	}
+	public void setLast_up_name(String last_up_name) {
+		this.last_up_name = last_up_name;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ xmzhProject/src/com/gotop/Generalprocess/model/ProcessModelTwo.java
@@ -0,0 +1,128 @@
+package com.gotop.Generalprocess.model;
+
+import java.io.Serializable;
+
+public class ProcessModelTwo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer rec_id;
+	private Integer flow_Id;
+
+	private String Resource_type;
+	private String opninion_content;
+	private String operator_type;
+	private String operator;
+	private String operater_date;
+	private String operater_time;
+
+	private String node_id;
+	private String Node_name;
+	private String orgid;
+
+	private String next_opr_name;
+
+	public Integer getRec_id() {
+		return rec_id;
+	}
+
+	public void setRec_id(Integer rec_id) {
+		this.rec_id = rec_id;
+	}
+
+	public Integer getFlow_Id() {
+		return flow_Id;
+	}
+
+	public void setFlow_Id(Integer flow_Id) {
+		this.flow_Id = flow_Id;
+	}
+
+	public String getResource_type() {
+		return Resource_type;
+	}
+
+	public void setResource_type(String resource_type) {
+		Resource_type = resource_type;
+	}
+
+	public String getOpninion_content() {
+		return opninion_content;
+	}
+
+	public void setOpninion_content(String opninion_content) {
+		this.opninion_content = opninion_content;
+	}
+
+	public String getOperator_type() {
+		return operator_type;
+	}
+
+	public void setOperator_type(String operator_type) {
+		this.operator_type = operator_type;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public String getOperater_date() {
+		return operater_date;
+	}
+
+	public void setOperater_date(String operater_date) {
+		this.operater_date = operater_date;
+	}
+
+	public String getOperater_time() {
+		return operater_time;
+	}
+
+	public void setOperater_time(String operater_time) {
+		this.operater_time = operater_time;
+	}
+
+	public String getNode_id() {
+		return node_id;
+	}
+
+	public void setNode_id(String node_id) {
+		this.node_id = node_id;
+	}
+
+	public String getNode_name() {
+		return Node_name;
+	}
+
+	public void setNode_name(String node_name) {
+		Node_name = node_name;
+	}
+
+	public String getOrgid() {
+		return orgid;
+	}
+
+	public void setOrgid(String orgid) {
+		this.orgid = orgid;
+	}
+
+	public String getNext_opr_name() {
+		return next_opr_name;
+	}
+
+	public void setNext_opr_name(String next_opr_name) {
+		this.next_opr_name = next_opr_name;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
