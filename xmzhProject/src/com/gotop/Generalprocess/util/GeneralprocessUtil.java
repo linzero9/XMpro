c2221b4e3313e12d7705bc94ddfcb5ebea07226a
吴佳俊 <398907877@qq.com>
2015-11-18 15:45:39 +0800

    modify

--- xmzhProject/src/com/gotop/Generalprocess/util/GeneralprocessUtil.java
+++ xmzhProject/src/com/gotop/Generalprocess/util/GeneralprocessUtil.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.derby.tools.sysinfo;
+
 import com.gotop.Generalprocess.annonation.GeneralprocessField;
 import com.gotop.Generalprocess.annonation.GeneralprocessFieldBean;
 
@@ -29,8 +31,10 @@ public class GeneralprocessUtil<T> {
 		Field[] fs = classc.getDeclaredFields();
 
 		for (Field field : fs) {
-			if (field.isAnnotationPresent(GeneralprocessField.class)) {
-				
+		
+				if(field.isAnnotationPresent(GeneralprocessField.class)){
+					
+			
 				
 				String   fieldName= field.getName();//字段的名称，用于和字段的值匹配？
 
@@ -38,23 +42,51 @@ public class GeneralprocessUtil<T> {
 						.getAnnotation(GeneralprocessField.class);
 
 				GeneralprocessFieldBean bean = new GeneralprocessFieldBean();
-
-				bean.setDescription(annotone.description());
-				bean.setHandle(annotone.handle());
-				bean.setHidden(annotone.hidden());
-				bean.setName(annotone.name());
-				bean.setType(annotone.type());
-				bean.setUrl(annotone.url());
-				bean.setFieldName(fieldName);
-
+				
+				
+				if(annotone==null){
+					bean.setFieldName(fieldName);
+				}else{
+					bean.setDescription(annotone.description());
+					bean.setHandle(annotone.handle());
+					bean.setHidden(annotone.hidden());
+					bean.setName(annotone.name());
+					bean.setType(annotone.type());
+					bean.setUrl(annotone.url());
+					bean.setFieldName(fieldName);
+			
+				}
+				
+			
 				returnBase.add(bean);
-
-			}
+				}
 
 		}
 		
 		return returnBase;
 
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		GeneralprocessUtil  iii = new GeneralprocessUtil();
+		List<GeneralprocessFieldBean>   lists= iii.getBaseInfoByClassName("com.gotop.Generalprocess.model.ProcessModelOne");
+		
+		for (GeneralprocessFieldBean generalprocessFieldBean : lists) {
+		
+			
+			System.out.println(	generalprocessFieldBean.getName());
+			System.out.println(generalprocessFieldBean.getType());
+			System.out.println(generalprocessFieldBean.getHandle());
+			System.out.println(generalprocessFieldBean.getFieldName());
+			System.out.println(generalprocessFieldBean.isHidden());
+			
+		}
+		
+		
+				
+		
+		
 	}
 
 }
