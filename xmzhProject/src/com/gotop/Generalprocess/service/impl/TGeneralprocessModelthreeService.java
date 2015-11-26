56535a6c0d00ba96502b15ff451f6c25d938041b
杨艺祥 <171627084@qq.com>
2015-11-26 11:03:48 +0800

    模式三代码第二版

--- xmzhProject/src/com/gotop/Generalprocess/service/impl/TGeneralprocessModelthreeService.java
+++ xmzhProject/src/com/gotop/Generalprocess/service/impl/TGeneralprocessModelthreeService.java
@@ -1,9 +1,13 @@
 package com.gotop.Generalprocess.service.impl;
 
 import com.gotop.Generalprocess.dao.ITGeneralprocessModelthreeDAO;
+import com.gotop.Generalprocess.model.ProcessModelOne;
+import com.gotop.Generalprocess.model.ProcessModelThree;
 import com.gotop.Generalprocess.model.TGeneralprocessModelthree;
 import com.gotop.Generalprocess.model.TGeneralprocessModelthreeExample;
 import com.gotop.Generalprocess.service.ITGeneralprocessModelthreeService;
+import com.gotop.jbpm.dto.TaskAssgineeDto;
+import com.gotop.vo.system.MUOUserSession;
 import com.primeton.utils.Page;
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -40,111 +44,25 @@ public class TGeneralprocessModelthreeService implements ITGeneralprocessModelth
         return this.tGeneralprocessModelthreeDAO;
     }
 
-    /**
-     * 动态查询实例，分页查询数据并返回list
-     * @abatorgenerated
-     */
-    public List queryDataGrid(HashMap map, Page page) throws Exception {
-        TGeneralprocessModelthreeExample example = new TGeneralprocessModelthreeExample();
-        TGeneralprocessModelthreeExample.Criteria criteria = example.createCriteria();
-        example.setOracleStart(page.getBegin());
-        example.setOracleEnd(page.getBegin()+page.getLength());
-        TGeneralprocessModelthree record = new TGeneralprocessModelthree();
-        List list = tGeneralprocessModelthreeDAO.selectByExampleAndPage(record,example,page);
-        return list;
-    }
 
-    /**
-     * 插入单条记录
-     * @abatorgenerated
-     */
-    public void insert(TGeneralprocessModelthree obj) throws Exception {
-        this.tGeneralprocessModelthreeDAO.insert(obj);
-    }
+	@Override
+	public void handleModelThree(MUOUserSession muo,
+			ProcessModelThree modelThree, TaskAssgineeDto taskAssgineeDto) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    /**
-     * 删除单条记录
-     * @abatorgenerated
-     */
-    public void delete(TGeneralprocessModelthree obj) throws Exception {
-        
-        this.tGeneralprocessModelthreeDAO.deleteByPrimaryKey(key);
-    }
-
-    /**
-     * 批量更新数据
-     * @abatorgenerated
-     */
-    public void updateBatch(List abs) throws Exception {
-        if(abs==null){
-            	return;
-        }
-        for(int i=0;i<abs.size();i++){
-            	TGeneralprocessModelthree tObject = (TGeneralprocessModelthree)abs.get(i);
-            //  没有主键的表，请自行处理
-        }
-    }
-
-    /**
-     * 批量插入数据
-     * @abatorgenerated
-     */
-    public void insertBatch(List abs) throws Exception {
-        if(abs==null){
-            	return;
-        }
-        for(int i=0;i<abs.size();i++){
-            	TGeneralprocessModelthree tObject = (TGeneralprocessModelthree)abs.get(i);
-            	this.insert(tObject);
-        }
-    }
-
-    /**
-     * 批量删除数据
-     * @abatorgenerated
-     */
-    public void deleteBatch(List abs) throws Exception {
-        if(abs==null){
-            	return;
-        }
-        for(int i=0;i<abs.size();i++){
-            	TGeneralprocessModelthree tObject = (TGeneralprocessModelthree)abs.get(i);
-            	this.delete(tObject);
-        }
-    }
-
-    /**
-     * datacell方式批量更新数据
-     * @abatorgenerated
-     */
-    public void updateDataGrid(HashMap hmp) throws Exception {
-        this.tGeneralprocessModelthreeDAO.startBatch();
-        List insertEntities = (List) hmp.get("insertEntities");
-        List deleteEntities = (List) hmp.get("deleteEntities");
-        List updateEntities = (List) hmp.get("updateEntities");
-        this.updateBatch(updateEntities);
-        this.insertBatch(insertEntities);
-        this.deleteBatch(deleteEntities);
-        this.tGeneralprocessModelthreeDAO.executeBatch();
-    }
-
-    /**
-     * 查询所有数据并返回List
-     * @abatorgenerated
-     */
-    public List queryAllDataList(HashMap map) throws Exception {
-        TGeneralprocessModelthreeExample example = new TGeneralprocessModelthreeExample();
-        TGeneralprocessModelthreeExample.Criteria criteria = example.createCriteria();
-        List list = tGeneralprocessModelthreeDAO.selectByExample(example);
-        return list;
-    }
-
-    /**
-     * 分页方式查询列表数据
-     * @abatorgenerated
-     */
-    public List queryPageDataList(HashMap map, Page page) throws Exception {
-        List list = tGeneralprocessModelthreeDAO.selectByDynamic(map,page);
-        return list;
-    }
+	@Override
+	public ProcessModelThree queryModelThree(ProcessModelThree modelThree) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if(modelThree != null){
+			if(modelThree.getFlow_id() != null && !"".equals(modelThree.getFlow_id())){
+				map.put("flow_id", modelThree.getFlow_id());
+			}
+			if(modelThree.getTaskName() != null && !"".equals(modelThree.getTaskName())){
+				map.put("taskName", modelThree.getTaskName());
+			}
+		}
+		return this.tGeneralprocessModelthreeDAO.queryModelThree(map);
+	}
 }
